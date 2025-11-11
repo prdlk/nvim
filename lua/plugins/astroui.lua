e782476e93698ece7577477a1a22fe249daedda5
Prad Nukala <prad@sonr.io>
2025-11-11 17:42:30 -0500

    refactor(plugins): simplify astroui plugin configuration

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,44 +1,6 @@
 return {
   {
     "AstroNvim/astroui",
-    dependencies = {
-      {
-        "nvim-focus/focus.nvim",
-        version = false,
-        opts = {
-          enable = true,
-          commands = true,
-          autoresize = {
-            enable = true,
-            width = 0,
-            height = 0,
-            minwidth = 0,
-            minheight = 0,
-            focusedwindow_minwidth = 0,
-            focusedwindow_minheight = 0,
-            height_quickfix = 10,
-          },
-          split = {
-            bufnew = false,
-            tmux = false,
-          },
-          ui = {
-            number = false,
-            relativenumber = false,
-            hybridnumber = false,
-            absolutenumber_unfocussed = false,
-            cursorline = true,
-            cursorcolumn = false,
-            colorcolumn = {
-              enable = false,
-              list = "+1",
-            },
-            signcolumn = true,
-            winhighlight = false,
-          },
-        },
-      },
-    },
     ---@type AstroUIOpts
     opts = {
       colorscheme = "cyberdream",
