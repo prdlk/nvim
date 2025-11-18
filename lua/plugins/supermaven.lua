1f798f0da1618e47cadd9090a86921ffe5ff3734
Prad Nukala <prad@sonr.io>
2025-11-18 11:36:39 -0500

    fix(core): update supermaven-nvim keymap for accept suggestion to <C-l>

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require("supermaven-nvim").setup({
       keymaps = {
-        accept_suggestion = "<Tab>",
+        accept_suggestion = "<C-l>",
         clear_suggestion = "<C-]>",
         accept_word = "<C-j>",
       },
