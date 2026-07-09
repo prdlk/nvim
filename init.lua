a776e04e3562256ff0f17d4195453af1227e384b
Prad Nukala <prad@sonr.io>
2026-07-09 14:09:34 -0400

    refactor(core): remove image preview setup on init

--- init.lua
+++ init.lua
@@ -34,4 +34,3 @@ vim.opt.titlestring = "%t - Nvim"
 -- Ensure Nvim doesn't slow down on exit by resetting shell
 vim.cmd('set shell=sh') -- Prevents fish-incompatible lag when exiting nvim in fish/tmux
 
-require("image_preview").setup({})
